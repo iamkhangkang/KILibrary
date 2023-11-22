ddf2efd3aff8122c352d8539dce45c6bc898f5c9
Abdul Azim Bin Abdullah 4616 <4616@laksamanacollege.edu.bn>
2023-11-22 11:21:49 +0800

    Test Commit (Added Test View Activity)

---
+++ app/src/main/java/com/example/kilibrary/TestView.kt
@@ -0,0 +1,11 @@
+package com.example.kilibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_view)
+    }
+}
